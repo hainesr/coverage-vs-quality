ef91dcbcf8446847a53b91a10b5037fd12a92f12
Robert Haines <rhaines@manchester.ac.uk>
2016-05-26 11:28:04 +0100

    First cut of an analysis script.

---
+++ scripts/regression_combined.py
@@ -0,0 +1,52 @@
+#------------------------------------------------------------------------------
+# Copyright (c) 2015, 2016, The University of Manchester, UK.
+#
+# Licenced under BSD. See LICENCE for details.
+#
+# Authors: Arturo Wong, Robert Haines
+#------------------------------------------------------------------------------
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+
+def regression(file_name, second_index_quality, order):
+
+    COLUMN_SEPARATOR = ','
+    metrics_data = pd.DataFrame.from_csv(file_name, sep=COLUMN_SEPARATOR, header=None)
+    labels = ['',
+              'Test Coverage',
+              'Branch Coverage',
+              'Cyclomatic Complexity',
+              'Lines of Code',
+              'Number Of Methods',
+              'Improvement Lack of Cohesion in Methods',
+              'Lack of Documentation',
+              'Depth of Inheritance',
+              'Coupling Between Objects'
+             ]
+
+    x = [0,0]
+    ys = [0,0]
+    for i in range(0, 2):
+        x[i] = metrics_data[i + 1]
+        y = metrics_data[second_index_quality]
+
+        coeffs = np.polyfit(x[i], y, order)
+        polynomial = np.poly1d(coeffs)
+        ys[i] = polynomial(x[i])
+
+    plt.figure(second_index_quality - 2)
+    plt.xlabel("Coverage")
+    plt.ylabel(labels[second_index_quality])
+    plt.title(labels[second_index_quality])
+    plt.plot(x[0], ys[0])
+    plt.plot(x[1], ys[1])
+    plt.legend([labels[1], labels[2]], loc='upper right')
+
+
+file_path = '../data/project data/combined/Merge/Merge all projects.csv'
+for x in range(3,10):
+    regression(file_path, x, 1)
+    plt.show()
